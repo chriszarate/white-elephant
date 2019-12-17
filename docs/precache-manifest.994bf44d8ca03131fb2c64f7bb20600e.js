8d319762e58e37a6eb11f48b1150379f6bc77dfb
chriszarate <chris@zarate.org>
2019-12-17 14:27:41 -0500

    Add missing files

---
+++ docs/precache-manifest.994bf44d8ca03131fb2c64f7bb20600e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af122ea9b10d42d6ff18844483263166",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c091183b6c5266377772",
+    "url": "./static/css/main.4db6e486.chunk.css"
+  },
+  {
+    "revision": "2eb6619a06b57313a92f",
+    "url": "./static/js/2.f85c173a.chunk.js"
+  },
+  {
+    "revision": "c091183b6c5266377772",
+    "url": "./static/js/main.303b7a09.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
