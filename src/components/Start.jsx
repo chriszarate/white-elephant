e1bbf65d81d7aef6c608bc2411867651742061b5
chriszarate <chris@zarate.org>
2019-12-17 13:34:16 -0500

    Last turn

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Card, Dimmer } from 'semantic-ui-react';
 
 export default function ( { children, count, person, show, turn } ) {
+	const lastTurn = turn > count;
+	const description = 'Unwrap a wrapped gift OR steal an unwrapped gift (provided it is not “dead”).';
+	const lastTurnDecription = 'Participant #1 takes the last turn: Steal any gift (even if it’s “dead”)!';
+
 	return (
 		<Dimmer active={show}>
 			<Card
 				className="card"
-				description="Unwrap a wrapped gift OR steal an unwrapped gift (provided it is not “dead”)."
+				description={lastTurn ? lastTurnDecription : description}
 				header={person}
 				image={`https://api.adorable.io/avatars/320/${person}.png`}
-				meta={`${turn} / ${count}`}
+				meta={lastTurn ? 'Last turn!' : `${turn} / ${count}`}
 				extra={children}
 			/>
 		</Dimmer>
